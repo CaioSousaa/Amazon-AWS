dcabef2ad5faac6a8cedd495fe2fea58baaff1e0
CaioSousaa <caiothalles607@gmail.com>
2025-03-14 16:21:16 -0300

    fix: fixing bug

--- amazon-rds/apps/purchase/src/modules/product/product.module.ts
+++ amazon-rds/apps/purchase/src/modules/product/product.module.ts
@@ -2,7 +2,6 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { CreateProductService } from './services/CreateProduct.service';
 import { ProductPrismaRepository } from 'src/external/repositories/ProductPrismaRepository';
 import { ProductController } from './infra/http/product.controller';
-import { UpdateProductService } from './services/UpdateProduct.service';
 import { FindAllProductsService } from './services/FindAllProducts.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
@@ -21,7 +20,6 @@ export class ProductModule {
             configService.get('STRIPE_SECRET_KEY'),
           inject: [ConfigService],
         },
-        UpdateProductService,
         ProductPrismaRepository,
         FindAllProductsService,
       ],
